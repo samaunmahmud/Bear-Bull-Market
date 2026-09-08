9c6902bf3738928c11aca2eed600d9855cb8bc94
samaunmahmud <samaunmahmud9@gmail.com>
2026-09-08 20:27:16 +0100

    feat(security): add SecurityConfig and InMemory UserDetails configuration for basic auth

---
+++ src/main/java/org/alphaspring/store/security/UserDetailsConfig.java
@@ -0,0 +1,41 @@
+package org.alphaspring.store.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+@Configuration
+public class UserDetailsConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails normalUser = User.builder()
+                .username("samaun")
+                .password(passwordEncoder.encode("password123"))
+                        .roles("USER")
+                        .build();
+
+
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("adminpass"))
+                .roles("USER")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+        }
+
+        }
+
+
